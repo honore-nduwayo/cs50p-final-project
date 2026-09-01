f2e1ad57f1dc133232ce054d1a1f6a9ad04f56d4
honore-nduwayo <nduwayohonore@gmail.com>
2026-09-01 11:45:23 +0200

    setup

---
+++ project.py
@@ -0,0 +1,50 @@
+"""
+CS50 Final Project
+Name: Honoré Nduwayo
+Country: Burundi
+
+"""
+# Importing libraries/requirements
+import re
+import argparse
+import datetime
+import collections
+import sys
+
+
+##### Project starts here #####
+
+class EntryLog:
+    def __init__(sefl, ):
+        ...
+
+def main():
+    ...
+def get_args():
+    ...
+
+def parse_log_line(line):
+    ...
+def load_log_file(filepath):
+    ...
+def detect_brute_force(entries, threshold, time_window_seconds):
+    ...
+def detect_port_scan(entries, threshold, time_window_seconds):
+    ...
+
+def detect_unusual_hours(entries, normal_start_hour, normal_end_hour):
+    ...
+def calculate_severity(finding):
+    ...
+
+def generate_report(findings):
+    ...
+
+def save_report(report_text, output_path):
+    ...
+    
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ test_project.py
@@ -0,0 +1,4 @@
+import pytest
+
+# The 3 functions to test/assert
+from project import parse_log_line, calculate_severity, detect_brute_force
