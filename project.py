a4cd8d3ee13ad7472f431153bce1e5a85fcc25bb
honore-nduwayo <nduwayohonore@gmail.com>
2026-09-01 14:00:50 +0200

    Milestone on  EntryLog class

--- project.py
+++ project.py
@@ -14,8 +14,30 @@ import sys
 ##### Project starts here #####
 
 class EntryLog:
-    def __init__(sefl, ):
-        ...
+    def __init__(self, timestamp, ip, event_type, port, user):
+        self.timestamp = timestamp
+        self.ip = ip
+        self.event_type = event_type
+        self.port = port
+        self.user = user
+
+    def is_failed_login(self):
+        return self.event_type == "failed_login"
+    
+    def is_success_login(self):
+        return self.event_type == "success_login"
+    
+    def is_connection_attempt(self):
+        return self.event_type == "connection_attempt"
+
+
+entry1 = EntryLog(20, "203.0.113.45", "failed_login", 51422, None)
+entry2 = EntryLog(30, "255.255.255.01", "success_login", 4545, None)
+
+print(f"Event1: {entry1.ip}:{entry1.event_type} ({entry1.is_failed_login})")
+print(f"Event2: {entry2.ip}:{entry2.event_type} ({entry2.is_failed_login})")
+
+#{"timestamp": some_datetime, "ip": "203.0.113.45", "event_type": "failed_login", "port": 51422, "user": None}
 
 def main():
     ...
